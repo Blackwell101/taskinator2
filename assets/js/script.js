31011d0d2fa3d5472aedb2d68f1e4b3ef6f76c95
Blackwell101 <103456893+Blackwell101@users.noreply.github.com>
2022-06-27 16:29:47 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,7 @@ var formEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-
     event.preventDefault();
-
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
